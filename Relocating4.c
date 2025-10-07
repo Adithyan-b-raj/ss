ecd16a8ea0e65e0b99d52d7a0a2100c2946b0cd1
adithyan <145749590+Adithyan-b-raj@users.noreply.github.com>
2025-10-07 10:46:54 +0530

    Create Relocating4.c

---
+++ Relocating4.c
@@ -0,0 +1,26 @@
+   case'5': strcat(bit,"101");
+        break;
+        case'6': strcat(bit,"110");
+        break;
+        case'7': strcat(bit,"111");
+        break;
+        case'8': strcat(bit,"1000");
+        break;
+        case'9': strcat(bit,"1001");
+        break;
+        case'A': strcat(bit,"1010");
+        break;
+        case'B': strcat(bit,"1011");
+        break;
+        case'C': strcat(bit,"1100");
+        break;
+        case'D': strcat(bit,"1101");
+        break;
+        case'E': strcat(bit,"1110");
+        break;
+        case'F': strcat(bit,"1111");
+        break;
+        }
+    }
+}
+  
